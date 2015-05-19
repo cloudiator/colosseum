6baba1710e9a368e7f386f1a8a0028fcaabce27c
Daniel Seybold <daniel.seybold@uni-ulm.de>
2015-05-19 16:25:32 +0200

    added an abstract installer

---
+++ app/components/installer/AbstractInstaller.java
@@ -0,0 +1,19 @@
+package components.installer;
+
+/**
+ * Created by Daniel Seybold on 19.05.2015.
+ */
+abstract class AbstractInstaller {
+
+    public AbstractInstaller(){
+
+    }
+
+    protected void initDownloads(){
+
+    }
+
+    protected void downloadSources(){
+
+    }
+}

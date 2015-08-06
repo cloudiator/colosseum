e2f672c9e8622ebaca6e345f1ad25e3c1254edfa
Daniel Seybold <daniel.seybold@uni-ulm.de>
2015-08-06 16:04:07 +0200

    PS-71 fixed Kairosdb installation for WindowsInstaller

--- app/components/installer/WindowsInstaller.java
+++ app/components/installer/WindowsInstaller.java
@@ -31,11 +31,14 @@ import play.Play;
 public class WindowsInstaller extends AbstractInstaller {
     private final String homeDir;
     private static final String JAVA_EXE = "jre8.exe";
-    private static final String SEVEN_ZIP_ARCHIVE = "7zip.zip";
+    private static final String SEVEN_ZIP_ARCHIVE = "7za920.zip";
     private static final String SEVEN_ZIP_DIR = "7zip";
+    private static final String SEVEN_ZIP_EXE = "7za.exe";
     private static final String VISOR_BAT = "startVisor.bat";
+    private static final String KAIROSDB_BAT = "startKairos.bat";
     private static final String JAVA_DOWNLOAD = Play.application().configuration().getString("colosseum.installer.windows.java.download");
-    private static final String SEVEN_ZIP_DOWNLOAD = Play.application().configuration().getString("colosseum.installer.windows.java.download");
+    private static final String SEVEN_ZIP_DOWNLOAD = Play.application().configuration().getString("colosseum.installer.windows.7zip.download");
+
 
 
     public WindowsInstaller(RemoteConnection remoteConnection, VirtualMachine virtualMachine, Tenant tenant) {
@@ -67,7 +70,7 @@ public class WindowsInstaller extends AbstractInstaller {
         //Set JAVA envirnonment vars, use SETX for setting the vars for all future session use /m for machine scope
         remoteConnection.executeCommand("SETX PATH %PATH%;" + this.homeDir + "\\" + WindowsInstaller.JAVA_DIR + "\\bin /m");
         remoteConnection.executeCommand("SETX JAVA_HOME " + this.homeDir + "\\" + WindowsInstaller.JAVA_DIR + " /m");
-       
+
         Logger.debug("Java successfully installed!");
 
 
@@ -75,7 +78,7 @@ public class WindowsInstaller extends AbstractInstaller {
 
     private void install7Zip(){
         Logger.debug("Unzipping 7zip...");
-        this.remoteConnection.executeCommand("powershell -command & { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('" + this.homeDir + "7zip.zip', '" + this.homeDir + "\\"+ this.SEVEN_ZIP_DIR + "'); }");
+        this.remoteConnection.executeCommand("powershell -command & { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('" + this.homeDir + "\\" + WindowsInstaller.SEVEN_ZIP_ARCHIVE +"', '" + this.homeDir + "\\" + WindowsInstaller.SEVEN_ZIP_DIR +  "'); }");
         Logger.debug("7zip successfully unzipped!");
     }
 
@@ -98,6 +101,7 @@ public class WindowsInstaller extends AbstractInstaller {
 
         //create schtaks
         this.remoteConnection.executeCommand("schtasks.exe /create /st 00:00  /sc ONCE /tn " + visorJobId + " /tr \"java -jar " + this.homeDir + "\\" + WindowsInstaller.VISOR_BAT + "\"");
+        this.waitForSchtaskCreation();
         //run schtask
         this.remoteConnection.executeCommand("schtasks.exe /run /tn " + visorJobId );
 
@@ -110,16 +114,21 @@ public class WindowsInstaller extends AbstractInstaller {
 
         Logger.debug("Extract, setup and start KairosDB...");
         //extract kairosdb
-        this.remoteConnection.executeCommand("powershell -command " + this.homeDir +"\\"+ WindowsInstaller.SEVEN_ZIP_DIR +"\\7za.exe e " + this.homeDir + "\\"+ WindowsInstaller.KAIROSDB_ARCHIVE + " -o" + this.homeDir);
+        this.remoteConnection.executeCommand("powershell -command " + this.homeDir + "\\" + WindowsInstaller.SEVEN_ZIP_DIR + "\\" + WindowsInstaller.SEVEN_ZIP_EXE + " e " + this.homeDir + "\\" + WindowsInstaller.KAIROSDB_ARCHIVE + " -o" + this.homeDir);
         String kairosDbTar = WindowsInstaller.KAIROSDB_ARCHIVE.replace(".gz", "");
-        this.remoteConnection.executeCommand("powershell -command " + this.homeDir + "\\" + WindowsInstaller.SEVEN_ZIP_DIR + "\7za.exe x " + this.homeDir + "\\" + kairosDbTar + " -o" + this.homeDir);
+        this.remoteConnection.executeCommand("powershell -command " + this.homeDir + "\\" + WindowsInstaller.SEVEN_ZIP_DIR + "\\" +WindowsInstaller.SEVEN_ZIP_EXE + " x " + this.homeDir + "\\" + kairosDbTar + " -o" + this.homeDir);
 
         //set firewall rule
         this.remoteConnection.executeCommand("powershell -command netsh advfirewall firewall add rule name = 'Open Kairos Port 8080' dir = in action = allow protocol=TCP localport=8080");
 
+        //create a .bat file to start kairosDB, because it is not possible to pass schtasks paramters using overthere
+        String startCommand = this.homeDir + "\\" + WindowsInstaller.KAIRROSDB_DIR + "\\bin\\kairosdb.bat run";
+        this.remoteConnection.writeFile(this.homeDir + "\\" + WindowsInstaller.KAIROSDB_BAT, startCommand, false );
+
         //start kairosdb in backround
         String kairosJobId = "kairosDB";
-        this.remoteConnection.executeCommand("schtasks.exe /create /st 00:00  /sc ONCE /tn "+ kairosJobId +" /tr \""+this.homeDir+"\\kairosdb\\bin\\kairosdb.bat run\"");
+        this.remoteConnection.executeCommand("schtasks.exe /create /st 00:00  /sc ONCE /tn " + kairosJobId + " /tr \"" + this.homeDir + "\\" + WindowsInstaller.KAIROSDB_BAT + "\"");
+        this.waitForSchtaskCreation();
         this.remoteConnection.executeCommand("schtasks.exe /run /tn " + kairosJobId);
         Logger.debug("KairosDB successfully started!");
 
@@ -149,5 +158,15 @@ public class WindowsInstaller extends AbstractInstaller {
         this.finishInstallation();
 
 
+    }
+
+    private void waitForSchtaskCreation(){
+        //Sleep 5 seconds to make sure the schtask creation is finished
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            Logger.error("Error while waiting for schtask to be created!", e);
+            e.printStackTrace();
+        }
     }
 }

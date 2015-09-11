d1b1b970bad06ba0ded1228ac1b57efefe72200c
Christopher Hauser <christopher.hauser@uni-ulm.de>
2015-09-11 14:05:05 +0200

    removed molpro-gui from master

--- app/views/helper/bootstrap/fields.scala
+++
@@ -1,8 +0,0 @@
-package views.helper.bootstrap
-
-import views.html.helper.bootstrap._
-import views.html.helper.FieldConstructor
-
-object BasicField {
-  implicit val bootstrapConstructor = FieldConstructor(basicField.f)
-}

3340ec3d87c210ec9acb759291edfe9f4904d312
Daniel Baur <daniel.baur@uni-ulm.de>
2016-03-09 12:32:50 +0100

    Updated fix_mtu script

--- resources/fix_mtu.sh
+++ resources/fix_mtu.sh
@@ -8,4 +8,10 @@ MTU=`ifconfig ${INTERFACE} | awk -F: '/MTU/{print $2+0}'`
 # default docker file exists
 if [ -f /etc/default/docker ]; then
     echo DOCKER_OPTS="--mtu=${MTU}" | tee --append /etc/default/docker > /dev/null
+
+    # we need to set environment file, as later ubuntu systemd are skipping the default file...
+    if [ -f /lib/systemd/system/docker.service ]; then
+        perl -i -pe "BEGIN{undef $/;} s/^\[Service\]$/[Service]\nEnvironmentFile=-\/etc\/default\/docker/sgm" /lib/systemd/system/docker.service
+        systemctl daemon-reload
+    fi
 fi

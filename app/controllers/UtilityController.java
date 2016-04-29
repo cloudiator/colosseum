ff9926bed16d7cf9603ee9404fa889798c4f05b2
Daniel Baur <daniel.baur@uni-ulm.de>
2016-04-29 16:34:49 +0200

    Added utility method to access current configuration via json

--- app/controllers/UtilityController.java
+++ app/controllers/UtilityController.java
@@ -1,21 +1,39 @@
 package controllers;
 
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.common.io.Files;
 import com.google.inject.Inject;
+import com.typesafe.config.ConfigValue;
+import controllers.security.SecuredSessionOrToken;
+import play.Play;
+import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
+import play.mvc.Security;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.util.Map;
 
 /**
  * Created by daniel on 28.04.16.
  */
-public class UtilityController extends Controller {
+@Security.Authenticated(SecuredSessionOrToken.class) public class UtilityController
+    extends Controller {
 
     @Inject public UtilityController() {
 
+    }
+
+    public Result configuration() {
+        final java.util.Set<Map.Entry<String, ConfigValue>> entries =
+            Play.application().configuration().getConfig("colosseum").entrySet();
+        ObjectNode result = Json.newObject();
+        for (Map.Entry<String, ConfigValue> entry : entries) {
+            result.put(entry.getKey(), entry.getValue().render());
+        }
+        return ok(result);
     }
 
     public Result version() {
@@ -28,7 +46,7 @@ public class UtilityController extends Controller {
             try {
                 //validate version
                 int validate = Integer.valueOf(version);
-                if(validate == 0) {
+                if (validate == 0) {
                     return notFound("Version number not available.");
                 }
             } catch (NumberFormatException ignored) {

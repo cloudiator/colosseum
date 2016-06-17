85b68cb57f73449bb1e920951f257a309befb5e5
Daniel Baur <dbaur@users.noreply.github.com>
2016-06-17 15:12:10 +0200

    PS-174 Implemented log collection (lance, colosseum) (#55)
    * PS-174 started work on log collection
    
    * PS-174 started work on log collection
    
    * PS-174 more work on log collector
    
    * PS-174 started work on log collection
    
    * PS-174 more work on log collector
    
    * PS-174 finalized logic for linux
    
    * PS-174 moved homedir function to vendor type
    
    * PS-174 made log location configurable

---
+++ app/components/log/ColosseumLogFile.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2014-2016 University of Ulm
+ *
+ * See the NOTICE file distributed with this work for additional information
+ * regarding copyright ownership.  Licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package components.log;
+
+import play.Play;
+
+import java.io.File;
+
+/**
+ * Created by daniel on 16.06.16.
+ */
+public class ColosseumLogFile extends LocalLogFile {
+
+    private static final String COLOSSEUM_LOG_PATH =
+        Play.application().configuration().getString("colosseum.log.colosseumLogPath");
+
+    @Override protected File file() {
+        return new File(COLOSSEUM_LOG_PATH);
+    }
+}
